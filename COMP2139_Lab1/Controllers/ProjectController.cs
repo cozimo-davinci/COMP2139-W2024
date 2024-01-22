1e3275bdb5223df75639dc751515e1b3cdfc8a39
cozimo-davinci <cozimodeaudiatore@gmail.com>
2024-01-21 19:31:14 -0500

    Complete Week 2 - Lab 2

---
+++ COMP2139_Lab1/Controllers/ProjectController.cs
@@ -0,0 +1,38 @@
+﻿using COMP2139_Lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP2139_Lab1.Controllers
+{
+    public class ProjectController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var projects = new List<Project>()
+            {
+                new Project {projectID = 1, Name = "Project 1", Description = "My First Project"}
+            };
+
+            return View(projects);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Project project)
+        {
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var project = new Project { projectID = id, Name = "Project " + id, Description = "Details of project " + id };
+            return View(project);
+        }
+    }
+}
---
+++ COMP2139_Lab1/Models/Project.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP2139_Lab1.Models
+{
+    public class Project
+    {
+        [Key]
+        public int projectID {  get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        public string? Status { get; set; }
+
+
+    }
+}
